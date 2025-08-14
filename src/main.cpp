e1381bb0b8288fedb8b332689dc84538aaef0fdb
Hasan Alp Doyduk <hasanalptr@gmail.com>
2025-08-14 20:47:33 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <Arduino.h>
 #include "rf_module.h"
+#include <U8glib.h>
+
+// 
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_DEV_0 | U8G_I2C_OPT_NO_ACK | U8G_I2C_OPT_FAST); // Fast I2C / TWI
 
 
 // put function declarations here:
